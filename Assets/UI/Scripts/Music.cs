c47e937e6fd61c5e6de0e98d32612687fa211250
Jazib Ali <m.mianjazibali@gmail.com>
2019-01-09 18:25:14 +0500

    Background Music Setup

---
+++ Assets/UI/Scripts/Music.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Scene")
+        {
+            _audioSource.Pause();
+            //Destroy(this.gameObject);
+        }
+        else
+        {
+            if (_audioSource.isPlaying)
+            {
+                return;
+            }
+            _audioSource.UnPause();
+        }
+    }
+}

dbba71d74497ee04cb6dd3d72b7d56b27bc86d89
Mian Jazib Ali <m.mianjazibali@gmail.com>
2019-01-23 15:43:23 +0500

    Chapter AutoScroll Effect

---
+++ Assets/UI/Scripts/AutoScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroll : MonoBehaviour
+{
+    int levelReached;
+
+    void Awake()
+    {
+        levelReached = PlayerPrefs.GetInt("levelReached");
+        Debug.Log(levelReached);
+        if (levelReached >= 19)
+        {
+            GetComponent<RectTransform>().localPosition = new Vector3(-1100, 0, 0);
+        }
+        else
+        if (levelReached >= 10)
+        {
+            GetComponent<RectTransform>().localPosition = new Vector3(-550, 0, 0);
+        }
+        else
+        {
+            GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+        }
+        
+    }
+}

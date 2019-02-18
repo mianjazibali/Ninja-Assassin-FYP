b058578d739d37e1546af6b44b2b07c3b92ec985
Mian Jazib Ali <m.mianjazibali.com>
2019-02-18 11:32:10 +0500

    Shuriken Script Optimized

--- Assets/Scripts/Shuriken.cs
+++ Assets/Scripts/Shuriken.cs
@@ -5,40 +5,8 @@ using UnityEngine;
 public class Shuriken : MonoBehaviour
 {
     public float damagePower = 10f;
-    public float moveSpeed;
-    public float rotationSpeed;
 
     private Rigidbody myRigidbody;
-
-    private Vector3 direction;
-
-    // Use this for initialization
-    void Start()
-    {
-        myRigidbody = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void FixedUpdate()
-    {
-        myRigidbody.velocity = direction * moveSpeed;
-        transform.Rotate(new Vector3(0, 100, 0) * Time.deltaTime * rotationSpeed);
-    }
-
-    public void setMoveSpeed(float speed)
-    {
-        moveSpeed = speed;
-    }
-
-    public void Initialize(Vector3 direction)
-    {
-        this.direction = direction;
-    }
 
     private void OnBecameInvisible()
     {

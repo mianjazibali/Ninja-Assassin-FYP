babc780695aedcf5032bcc78110a89c96eceffca
Mian Jazib Ali <m.mianjazibali@gmail.com>
2019-03-16 19:49:43 +0500

    Explode Effect After Destruction

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    private void Start()
+    {
+        Explodee();
+    }
+
+    void Explodee()
+    {
+        foreach(Transform t in transform)
+        {
+            Rigidbody rb = t.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+        }
+    }
+}

5684b52eb801b3f7a1a34b003a0a7cb7f16f0086
Mian Jazib Ali <m.mianjazibali@gmail.com>
2019-03-20 12:56:02 +0500

    Bear Trap Working

---
+++ Assets/Scripts/BearTrap.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+    public float damage = 10f;
+    public float damageRate = 1f;
+    public float trappedTime = 3f;
+
+    float nextDamage;
+
+    PlayerHealth playerHealth;
+    Movement playerMovement;
+    bool isTrapped = false;
+
+    void Start()
+    {
+        nextDamage = Time.time;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        playerMovement = player.GetComponent<Movement>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isTrapped)
+        {
+            isTrapped = true;
+            playerMovement.BlockMovement();
+            playerMovement.Attack();
+            playerHealth.GetComponent<Animator>().SetBool("Trapped", isTrapped);
+            StartCoroutine(DestroyTrap());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (isTrapped && nextDamage <= Time.time)
+        {
+            playerHealth.AddDamage(damage);
+            nextDamage = Time.time + damageRate;
+        }
+    }
+
+    IEnumerator DestroyTrap()
+    {
+        yield return new WaitForSeconds(trappedTime - 1);
+        isTrapped = false;
+        playerHealth.GetComponent<Animator>().SetBool("Trapped", isTrapped);
+        Destroy(gameObject);
+    }
+}

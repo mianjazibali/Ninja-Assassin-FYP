465fbef211499c659f60e1e0944bac886da608ab
Jazib Ali <m.mianjazibali@gmail.com>
2019-04-14 02:41:19 -0700

    Moving Platform Script Added

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent.SetParent(transform, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent.SetParent(null, true);
+        }
+    }
+}

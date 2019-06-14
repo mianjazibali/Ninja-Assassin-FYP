f50e14073cb50bc434036e5a94b33f832a48eda4
Jazib Ali <m.mianjazibali@gmail.com>
2019-06-14 19:59:08 +0500

    Level 01 Designing Till Fountain

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     private LevelManager levelManager;
+    public Material activeMaterial;
+    private bool isCheckpointUsed;
 
     private void Start()
     {
+        isCheckpointUsed = false;
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && !isCheckpointUsed)
         {
             levelManager.lastCheckpointPosition = new Vector3 (transform.position.x, levelManager.lastCheckpointPosition.y, levelManager.lastCheckpointPosition.z);
+            GetComponent<Renderer>().material = activeMaterial;
+            isCheckpointUsed = true;
         }
     }
 }

8c21497a54bfc40b4adedbde08c8bcb4705e1dfa
Mian Jazib Ali <m.mianjazibali@gmail.com>
2019-07-02 22:34:20 +0500

    Coins Show/Save Prefs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,14 +6,28 @@ public class LevelManager : MonoBehaviour
 {
     public Vector3 lastCheckpointPosition;
 
-    [SerializeField]
-    private int scrollCount = 0;
-    [SerializeField]
+    public GameObject coinsText;
+    public GameObject scrollText;
+
+    private int scrollCount;
     private int coinCount;
+
+    private TMPro.TextMeshProUGUI coinTMPro;
 
     private void Start()
     {
         lastCheckpointPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        coinTMPro = coinsText.GetComponent<TMPro.TextMeshProUGUI>();
+        if (PlayerPrefs.GetInt("totalCoins") == 0)
+        {
+            PlayerPrefs.SetInt("totalCoins", coinCount);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            coinCount = PlayerPrefs.GetInt("totalCoins");
+        }
+        coinTMPro.text = coinCount.ToString("D6");
     }
 
     public void IncrementScroll()
@@ -24,5 +38,13 @@ public class LevelManager : MonoBehaviour
     public void SetCoins(int coins)
     {
         coinCount = coinCount + coins;
+        SaveCoins();
+    }
+
+    public void SaveCoins()
+    {
+        coinTMPro.text = coinCount.ToString("D6");
+        PlayerPrefs.SetInt("totalCoins", coinCount);
+        PlayerPrefs.Save();
     }
 }

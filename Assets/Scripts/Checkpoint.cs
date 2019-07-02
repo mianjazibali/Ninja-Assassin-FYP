43e0a6a0187eccf9bc0895dc7950d4841e306c25
Mian Jazib Ali <m.mianjazibali@gmail.com>
2019-07-02 17:03:44 +0500

    Checkpoint Bug Resolved + Game Cleaners + Action Icons

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private LevelManager levelManager;
+    
     public Material activeMaterial;
+
+    private LevelManager levelManager;
+    private Vector3 respawnPosition;
     private bool isCheckpointUsed;
 
     private void Start()
     {
-        isCheckpointUsed = false;
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+        respawnPosition = transform.GetChild(0).position;
+        isCheckpointUsed = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player" && !isCheckpointUsed)
         {
-            levelManager.lastCheckpointPosition = new Vector3 (transform.position.x, levelManager.lastCheckpointPosition.y, levelManager.lastCheckpointPosition.z);
+            levelManager.lastCheckpointPosition = new Vector3 (respawnPosition.x, respawnPosition.y, levelManager.lastCheckpointPosition.z);
             GetComponent<Renderer>().material = activeMaterial;
             isCheckpointUsed = true;
         }

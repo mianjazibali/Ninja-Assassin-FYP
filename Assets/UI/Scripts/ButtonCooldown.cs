ffda38f7b041f27461307dd5413b771aaf132230
Mian Jazib Ali <m.mianjazibali@gmail.com>
2019-07-09 22:58:05 +0500

    All Upgrades Added

--- Assets/UI/Scripts/ButtonCooldown.cs
+++ Assets/UI/Scripts/ButtonCooldown.cs
@@ -5,7 +5,13 @@ public class ButtonCooldown : MonoBehaviour
 {
     public Transform LoadingBar;
     [SerializeField] private float currentAmount;
-    [SerializeField] private float time;
+    public enum Weapon
+    {
+        Shuriken,
+        Sword,
+    }
+    public Weapon weapon = Weapon.Shuriken;
+    [SerializeField] private float maxCooldownTime;
     [SerializeField] private float animationTime;
     private float speed;
 
@@ -15,7 +21,11 @@ public class ButtonCooldown : MonoBehaviour
     void Start()
     {
         LoadingBarImage = LoadingBar.GetComponent<Image>();
-        speed = 100 / (time + animationTime);
+        if (weapon == Weapon.Sword)
+            maxCooldownTime -= PlayerPrefs.GetInt("currentDash");
+        else
+            maxCooldownTime -= PlayerPrefs.GetInt("shurikenDash");
+        speed = 100 / (maxCooldownTime + animationTime);
     }
 
     private void OnEnable()
--- Assets/UI/Scripts/Upgrade.cs
+++ Assets/UI/Scripts/Upgrade.cs
@@ -9,10 +9,18 @@ public class Upgrade : MonoBehaviour
     public GameObject upgradeErrorText;
     public GameObject[] livesUpgradeBars;
     public GameObject[] livesUpgradeButtons;
+    public GameObject[] shurikenUpgradeBars;
+    public GameObject[] shurikenUpgradeButtons;
+    public GameObject[] dashUpgradeBars;
+    public GameObject[] dashUpgradeButtons;
 
     private int currentLives = 0;
+    private int currentShuriken = 0;
+    private int currentDash = 0;
     private int coinCount = 0;
     private int defaultLives = 3;
+    private int defaultShuriken = 3;
+    private int defaultDash = 1;
 
     private TMPro.TextMeshProUGUI coinsTMPro;
 
@@ -23,6 +31,12 @@ public class Upgrade : MonoBehaviour
         currentLives = defaultLives + PlayerPrefs.GetInt("currentLives"); //Default 0 to Add On Existing 3 Default Lives 
         DisplayPreviousUpgradedLives();
         livesUpgradeButtons[0].SetActive(false);
+        currentShuriken = defaultShuriken + PlayerPrefs.GetInt("currentShuriken");
+        DisplayPreviousUpgradedShuriken();
+        shurikenUpgradeButtons[0].SetActive(false);
+        currentDash = defaultDash + PlayerPrefs.GetInt("currentDash");
+        DisplayPreviousUpgradedDash();
+        dashUpgradeButtons[0].SetActive(false);
     }
 
     void UpdateCoinText()
@@ -60,13 +74,47 @@ public class Upgrade : MonoBehaviour
         livesUpgradeButtons[currentLives].SetActive(true);
     }
 
+    void DisplayPreviousUpgradedShuriken()
+    {
+        for (int i = 0; i < shurikenUpgradeBars.Length; i++)
+        {
+            if (i < currentShuriken)
+            {
+                shurikenUpgradeBars[i].SetActive(true);
+            }
+            else
+            {
+                shurikenUpgradeBars[i].SetActive(false);
+            }
+        }
+        shurikenUpgradeButtons[currentShuriken].SetActive(true);
+    }
+
+    void DisplayPreviousUpgradedDash()
+    {
+        for (int i = 0; i < dashUpgradeBars.Length; i++)
+        {
+            if (i < currentDash)
+            {
+                dashUpgradeBars[i].SetActive(true);
+            }
+            else
+            {
+                dashUpgradeBars[i].SetActive(false);
+            }
+        }
+        dashUpgradeButtons[currentDash].SetActive(true);
+    }
+
     public void UpgradeLives()
     {
         string costStr = livesUpgradeButtons[currentLives].transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text;
         int cost = Convert.ToInt32(costStr);
-        if(PlayerPrefs.GetInt("totalCoins") >= cost)
+        if(coinCount >= cost)
         {
             currentLives++;
+            coinCount -= cost;
+            PlayerPrefs.SetInt("totalCoins", coinCount);
             PlayerPrefs.SetInt("currentLives", currentLives - defaultLives);
             PlayerPrefs.Save();
         }
@@ -76,6 +124,51 @@ public class Upgrade : MonoBehaviour
         }
         livesUpgradeButtons[currentLives].SetActive(true);
         livesUpgradeBars[currentLives - 1].SetActive(true);
+        UpdateCoinText();
         //Debug.Log(PlayerPrefs.GetInt("currentLives"));
+    }
+
+    public void UpgradeShuriken()
+    {
+        string costStr = shurikenUpgradeButtons[currentShuriken].transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text;
+        int cost = Convert.ToInt32(costStr);
+        if (PlayerPrefs.GetInt("totalCoins") >= cost)
+        {
+            currentShuriken++;
+            coinCount -= cost;
+            PlayerPrefs.SetInt("totalCoins", coinCount);
+            PlayerPrefs.SetInt("currentDash", currentShuriken - defaultShuriken);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            upgradeErrorText.SetActive(true);
+        }
+        shurikenUpgradeButtons[currentDash].SetActive(true);
+        shurikenUpgradeBars[currentDash - 1].SetActive(true);
+        UpdateCoinText();
+        //Debug.Log(PlayerPrefs.GetInt("currentDash"));
+    }
+
+    public void UpgradeDash()
+    {
+        string costStr = dashUpgradeButtons[currentDash].transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text;
+        int cost = Convert.ToInt32(costStr);
+        if (PlayerPrefs.GetInt("totalCoins") >= cost)
+        {
+            currentDash++;
+            coinCount -= cost;
+            PlayerPrefs.SetInt("totalCoins", coinCount);
+            PlayerPrefs.SetInt("currentDash", currentDash - defaultDash);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            upgradeErrorText.SetActive(true);
+        }
+        dashUpgradeButtons[currentDash].SetActive(true);
+        dashUpgradeBars[currentDash - 1].SetActive(true);
+        UpdateCoinText();
+        //Debug.Log(PlayerPrefs.GetInt("currentDash"));
     }
 }
